6f282e5ddae3f3f13b1df409d2a1e51674e5eb13
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-02-02 14:39:29 -0800

    point 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list in ascending order
+ * @list: Pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	listint_t *current, *temp, *prev;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current->next;
+		prev = current->prev;
+		while (prev != NULL && prev->n > current->n)
+		{
+			if (prev->prev != NULL)
+			{
+				prev->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			current->prev = prev->prev;
+			prev->prev = current;
+			current->next = prev;
+			prev->next = temp;
+			print_list(*list);
+			prev = current->prev;
+		}
+		current = temp;
+	}
+}
--- sort.h
+++ sort.h
@@ -16,4 +16,5 @@ void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 #endif
