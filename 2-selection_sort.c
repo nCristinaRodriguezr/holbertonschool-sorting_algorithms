2c9a19c8af274baef98c9ed2d13ac6b38f694907
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-02-04 14:52:34 -0800

    point 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include "sort.h"
 /**
+ * swap - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
  * selection_sort - fuction that sorts an array of integers in ascending
  * order using the Selection sort algorithm
  * @array: Pointer to the array to be sorted.
